5b9e85797d30a6e7afd8982014f064a232a40d2b
laraeliasms <lara.geovana@sempreceub.com>
2025-09-26 10:50:51 -0300

    Add files via upload

---
+++ Produto (2).java
@@ -0,0 +1,24 @@
+public abstract class Produto {
+    private int codigo;
+    private String nome;
+    private double preco;
+    private Categoria categoria;
+
+    
+
+    public Produto(int codigo, String nome, double preco, Categoria categoria) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+}
+
+   public int getCodigo() { return codigo; }
+    public String getNome() { return nome; }
+    public double getPreco() { return preco; }
+    public Categoria getCategoria() { return categoria; }
+
+    public void setPreco(double preco) { this.preco = preco; }
+
+    public abstract void exibirDetalhes();
+}
