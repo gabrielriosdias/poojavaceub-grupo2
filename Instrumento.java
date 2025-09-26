07b0a39036f668816e127071c26b0ba588ef0445
laraeliasms <lara.geovana@sempreceub.com>
2025-09-26 10:49:44 -0300

    Add files via upload

---
+++ Instrumento.java
@@ -0,0 +1,15 @@
+public class Instrumento extends Produto {
+    private String tipo;
+    private String marca;
+
+    public Instrumento(int codigo, String nome, double preco, Categoria categoria, String tipo, String marca) {
+        super(codigo, nome, preco, categoria);
+        this.tipo = tipo;
+        this.marca = marca;
+    }
+
+    @Override
+    public void exibirDetalhes() {
+        System.out.println("Instrumento: " + getNome() + " - Marca: " + marca + " (" + tipo + ")");
+    }
+}
